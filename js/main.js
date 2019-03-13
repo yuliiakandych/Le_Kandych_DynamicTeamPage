a62058f96e9a9a7a390bcf962e8368a16e5398a2
Darren Le <darrenle98@gmail.com>
2019-03-13 04:04:11 -0400

    fixed bug and changed design

--- js/main.js
+++ js/main.js
@@ -7,24 +7,68 @@ let lan 	= document.querySelector('.lan')
 	lanSide = document.querySelector(".lanSide")
 	yuliia 	= document.querySelector('.yuliia')
 	yuliiaSide = document.querySelector(".yuliiaSide")
+	bioL		= document.querySelector(".bioL")
+	bioY		= document.querySelector(".bioY")
 
+
+$(".promoText").hide().fadeIn(2000)
+$(".front").hide().delay(100).fadeIn(1000)
+$(".yuliia").hide().delay(300).fadeIn(1000)
 
 lan.addEventListener("mouseover", ()=>
 {
-	yuliiaSide.style.opacity = "1"
+	yuliiaSide.style.display = "block"
+	
+
+	lan.addEventListener("mouseout", ()=>
+	{
+	yuliiaSide.style.display = "none"
+	})
 })
 
-lan.addEventListener("mouseout", ()=>
-{
-	yuliiaSide.style.opacity = "0"
-})
 
 yuliia.addEventListener("mouseover", ()=>
 {
-	lanSide.style.opacity = "1"
+	lanSide.style.display = "block"
+	
+	yuliia.addEventListener("mouseout", ()=>
+	{
+	lanSide.style.display = "none"
+	})
+
+
 })
 
-yuliia.addEventListener("mouseout", ()=>
-{
-	lanSide.style.opacity = "0"
-})
+
+
+
+yuliia.addEventListener("click",()=>
+	{
+		if(bioY.style.display === "block")
+		{
+			$(".bioY").fadeOut()
+		}
+		else
+		{
+			$(".bioL").hide()
+			$(".bioY").fadeIn()
+		}
+		
+	})
+
+lan.addEventListener("click",()=>
+	{
+		if(bioL.style.display === "block")
+		{
+			$(".bioL").fadeOut()
+		}
+		else
+		{
+			$(".bioY").hide()
+			$(".bioL").fadeIn()
+		}
+		
+	})
+
+
+
