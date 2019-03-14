03e81b5e40cda848aa3388b2c731533bcb13df8e
Darren Le <darrenle98@gmail.com>
2019-03-13 21:08:19 -0400

    added array and name effect

--- js/main.js
+++ js/main.js
@@ -3,15 +3,21 @@
 
 console.log("aaa")
 
-let lan 	= document.querySelector('.lan')
-	lanSide = document.querySelector(".lanSide")
-	yuliia 	= document.querySelector('.yuliia')
-	yuliiaSide = document.querySelector(".yuliiaSide")
-	bioL		= document.querySelector(".bioL")
-	bioY		= document.querySelector(".bioY")
+// set variables
+let lan 		= document.querySelector('.lan')
+	lanSide 	= document.querySelector(".lanSide")
+	yuliia 		= document.querySelector('.yuliia')
+	yuliiaSide 	= document.querySelector(".yuliiaSide")
+	bio 		= document.querySelector(".bio")
+	title		= document.querySelectorAll(".name")
+	front		= document.querySelectorAll(".front")
 
+	bioArray 	= [
+					"Hi, my name is Lan. I'm from Vietnam which is a pretty nice and friendly country. I am passionate about designs, movies, music and recently codes. I want to be a Front-End Developer after I finish school at Fanshawe College. Check out my Instagram for more information.",
+					"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident ullam in vitae modi quas neque beatae mollitia ab, illo sit, pariatur explicabo aut odio repellendus aperiam et, alias quod inventore."
+					]
 
-
+// hover effect for Lan
 lan.addEventListener("mouseover", ()=>
 {
 	yuliiaSide.style.display = "block"
@@ -23,7 +29,7 @@ lan.addEventListener("mouseover", ()=>
 	})
 })
 
-
+// hover effect for Yuliia
 yuliia.addEventListener("mouseover", ()=>
 {
 	lanSide.style.display = "block"
@@ -36,36 +42,41 @@ yuliia.addEventListener("mouseover", ()=>
 
 })
 
+// set the function when click on each image
+front.forEach((e,index) => e.addEventListener("click",function()
+{
+
+	// call out the bio in array
+	bio.style.opacity = "1"
+	bio.innerHTML = "<p>" + bioArray[index] + "</p>" + "<span class='close'>&times;</span>";
 
 
-
-yuliia.addEventListener("click",()=>
+	// set color for Lan
+	if(index == 0)
 	{
-		if(bioY.style.opacity === "1")
-		{
-			bioY.style.opacity = "0"
-		}
-		else
-		{
-			bioL.style.opacity = "0"
-			bioY.style.opacity = "1"
-		}
-		
+	bio.style.backgroundColor = "rgba(204, 18, 40, 0.8)"
+	title[1].style.color = "white"
+	title[index].style.color = "rgba(204, 18, 40, 0.8)"
+	}
+
+
+
+	// set color for Yuliia
+	if(index == 1)
+	{
+	bio.style.backgroundColor = "rgba(189, 8, 165, 0.8)"
+	title[index].style.color = "rgba(189, 8, 165, 0.8)"
+	title[0].style.color = "white"
+	}
+
+	// create close function
+	let	close		= document.querySelector(".close")
+	close.addEventListener("click",function(){ 
+		bio.style.opacity = "0"
 	})
 
-lan.addEventListener("click",()=>
-	{
-		if(bioL.style.opacity === "1")
-		{
-			bioL.style.opacity = "0"
-		}
-		else
-		{
-			bioY.style.opacity = "0"
-			bioL.style.opacity = "1"
-		}
 		
-	})
+}))
 
 
 
